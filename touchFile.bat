3a3b648807ca9e9165a8f505d931faecb4eda912
lm1000ares <lm1000_ares@hotmail.com>
2018-02-07 11:37:08 +0800

    update

---
+++ touchFile.bat
@@ -0,0 +1,12 @@
+copy /b 4.SBI +,,
+copy /b 4.SDB +,,
+copy /b TOELF600.SBI +,,
+copy /b TOELF600.SDB +,,
+copy /b "Copy of 4.SBI" +,,
+copy /b "Copy of 4.SDB" +,,
+copy /b "Copy of TOELF600.SBI" +,,
+copy /b "Copy of TOELF600.SDB" +,,
+
+git add .
+git commit -m "update"
+git push
